64faec4f78a4b00eae7b20acb6c7c5f8bcd06b26
Gabriel Robles <grobles@fi.uba.ar>
2021-06-06 17:47:08 -0300

    osc primitives wip

--- server/electronic_instrument_adapter/instrument/instrument.py
+++ server/electronic_instrument_adapter/instrument/instrument.py
@@ -37,14 +37,14 @@ class Oscilloscope(Instrument):
 
     def __str__(self):
         return "Oscilloscope:\n\t" \
-                    "Brand  : {}\n\t" \
-                    "Model  : {}\n\t" \
-                    "ID     : {}\n\t" \
-                    "Status : {}".format(
-                        self.brand,
-                        self.model,
-                        self.id,
-                        self.status)
+               "Brand  : {}\n\t" \
+               "Model  : {}\n\t" \
+               "ID     : {}\n\t" \
+               "Status : {}".format(
+            self.brand,
+            self.model,
+            self.id,
+            self.status)
 
     def as_dict(self):
         return {
@@ -68,14 +68,53 @@ class Oscilloscope(Instrument):
         }
 
     def set_initial_configuration(self):
-        self.set_volts_scale(1, self.configuration.volts_scale)
-        self.set_time_scale(self.configuration.time_scale)
+        raise Exception("NotImplementedException")
 
-    def set_volts_scale(self):
+    # General purpose oscilloscope primitives
+
+    def clear_status(self):
+        raise Exception("NotImplementedException")
+
+    def reset_settings(self):
+        raise Exception("NotImplementedException")
+
+    def get_identification(self):
+        raise Exception("NotImplementedException")
+
+    # Channel configuration primitives
+
+    def set_channel_probe(self, channel, probe):
+        raise Exception("NotImplementedException")
+
+    # Timebase primitives
+
+    def set_timebase_scale(self, seconds):
+        raise Exception("NotImplementedException")
+
+    def set_timebase_mode(self, mode):
+        raise Exception("NotImplementedException")
+
+    def set_timebase_x_channel(self, channel):
+        raise Exception("NotImplementedException")
+
+    def set_timebase_y_channel(self, channel):
+        raise Exception("NotImplementedException")
+
+    # Volts primitives
+
+    def set_volts_scale(self, channel, volts_scale):
+        self.device.write('CH' + str(channel) + ':VOLts ' + str(volts_scale))
+
+    # Acquisitions primitives
+
+    def get_is_in_acquisitions_state(self):
+        raise Exception("NotImplementedException")
+
+    def stop_acquisitions(self):
         raise Exception("NotImplementedException")
 
 
 class OscilloscopeConfiguration:
     def __init__(self, volts_scale=5, time_scale=0.000500):
         self.volts_scale = volts_scale  # in volts
-        self.time_scale = time_scale  # in seconds
+        self.time_scale = time_scale    # in seconds
--- server/electronic_instrument_adapter/instrument/oscilloscope/tektronix_tds1002b.py
+++ server/electronic_instrument_adapter/instrument/oscilloscope/tektronix_tds1002b.py
@@ -3,10 +3,66 @@ from ..instrument import Oscilloscope
 
 class Tektronix_TDS1002B(Oscilloscope):
     def __init__(self, id, brand, model):
+        self._available_timebase_modes = ["YT", "XY"]
+        self._available_channels = ["CH1", "CH2"]
+
         super(Tektronix_TDS1002B, self).__init__(id, brand, model)
+
+    def set_initial_configuration(self):
+        self.configuration.volts_scale = 0.5
+        self.configuration.time_scale = 0.000200
+
+        self.stop_acquisitions()
+        self.clear_status()
+
+        self.set_channel_probe(1, 1)
+        self.set_channel_probe(2, 1)
+
+        self.set_volts_scale(1, self.configuration.volts_scale)
+        self.set_volts_scale(2, self.configuration.volts_scale)
+
+        self.set_timebase_mode("YT")
+
+        self.set_timebase_x_channel("CH1")
+        self.set_timebase_y_channel("CH2")
+
+        self.set_timebase_scale(self.configuration.time_scale)
+
+        self.get_identification()
+
+    def clear_status(self):
+        self.device.write("*CLS")
+
+    def reset_settings(self):
+        self.device.write("*RST")
+
+    def get_identification(self):
+        return self.device.query("*IDN?")
+
+    def stop_acquisitions(self):
+        self.device.write("ACQuire:STATE STOP")
+
+    def get_is_in_acquisitions_state(self):
+        return self.device.query("*OPC?") == 1
 
     def set_volts_scale(self, channel, volts_scale):
         self.device.write('CH' + str(channel) + ':VOLts ' + str(volts_scale))
 
-    def set_time_scale(self, seconds):
+    def set_timebase_scale(self, seconds):
         self.device.write('HORizontal:MAIn:SCAle ' + str(seconds))
+
+    def set_channel_probe(self, channel, probe):
+        self.device.write('CH' + str(channel) + ':PRObe ' + str(probe))
+
+    def set_timebase_mode(self, mode):
+        if self._available_timebase_modes.__contains__(str(mode)):
+            self.device.write('DISplay:FORMat ' + str(mode))
+
+    def set_timebase_x_channel(self, channel):
+        if self._available_channels.__contains__(str(channel)):
+            self.device.write("MEASUrement:IMMed:SOUrce1 " + str(channel))
+
+    def set_timebase_y_channel(self, channel):
+        if self._available_channels.__contains__(str(channel)):
+            self.device.write("MEASUrement:IMMed:SOUrce2 " + str(channel))
+
