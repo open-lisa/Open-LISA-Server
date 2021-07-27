f305b67bfc6bec24d65e33a827ae26edd3c61113
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2021-07-27 20:47:49 -0300

    remove SDK from this repo

--- sdk/electronic_instrument_adapter_sdk/domain/exceptions/sdk_exception.py
+++
@@ -1,5 +0,0 @@
-class ElectronicInstrumentAdapterException(Exception):
-  """ Base exception class """
-  def __init__(self, message):
-    self.message = message
-    super().__init__(message)
--- sdk/instal_sdk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source venv/bin/activate
-pip install wheel
-python setup.py bdist_wheel
-pip install dist/electronic_instrument_adapter_sdk-0.1.0-py3-none-any.whl --force-reinstall
