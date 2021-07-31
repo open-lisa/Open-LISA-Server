511888eb756acad233d733ed846c65ac700d7258
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2021-07-31 19:05:49 -0300

    fix command injection

--- server/electronic_instrument_adapter/instrument/instrument.py
+++ server/electronic_instrument_adapter/instrument/instrument.py
@@ -85,7 +85,7 @@ class Instrument:
         command_type = self.commands_map[command_base]['type']
 
         # Inject command parameters
-        self.__generate_command_with_injected_params(command)
+        command = self.__generate_command_with_injected_params(command)
 
         if command_type == COMMAND_TYPE_SET:
             written_bytes = self.device.write(self.commands_map[command_base]['command'])
