7dccea70c12734d0e2069af2a3955096a620f83e
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-06-11 23:25:34 -0300

    refactor base class Command

--- open_lisa/domain/command/command.py
+++ open_lisa/domain/command/command.py
@@ -1,3 +1,25 @@
+from enum import Enum
+
+from open_lisa.domain.command.command_parameters import CommandParameters
+
+
+class CommandType(Enum):
+    SCPI = "scpi"
+    CLIB = "clib"
+
+    def __str__(self):
+        return self.value
+
+
 class Command:
-  def execute(self, params):
-    raise NotImplementedError("Please Implement this method: execute")
+    def __init__(self, name, command, parameters, type, description) -> None:
+        self.name = name
+        self.command = command
+        self.type = type
+        self.description = description
+
+        assert isinstance(parameters, CommandParameters)
+        self.parameters = parameters
+
+    def execute(self, params):
+        raise NotImplementedError("Please Implement this method: execute")
