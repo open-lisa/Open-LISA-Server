88776e1246d6d559964f18ba51bdc3126dc6c2c3
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-06-16 20:40:46 -0300

    create instrument repository v2

---
+++ open_lisa/repositories/tests/test_instruments_repository.py
@@ -0,0 +1,11 @@
+def test_instruments_repository_should_be_created_with_the_entries_of_json_file():
+    pass
+
+
+def test_get_all_should_return_instruments_objects_at_least_one_for_each_instrument_registered_in_json_file():
+    pass
+
+
+def test_get_all_should_return_available_instruments_if_pyvisa_detects_them():
+    pass
+    # TODO: pyvisa should be mocked
