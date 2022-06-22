4ce5bb1f68dc38c4345e50273c957fb4d5149586
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-06-21 22:39:50 -0300

    implement CommandReturn to ctype mapping

--- open_lisa/domain/command/command_return.py
+++ open_lisa/domain/command/command_return.py
@@ -1,5 +1,5 @@
+import ctypes
 from enum import Enum
-
 from open_lisa.exceptions.invalid_command_parameter_value_error import InvalidCommandParameterValueError
 
 
@@ -37,3 +37,17 @@ class CommandReturn():
             "type": str(self.type),
             "description": self.description
         }
+
+    def to_ctype(self):
+        if self.type == CommandReturnType.INT:
+            return ctypes.c_int
+        elif self.type == CommandReturnType.FLOAT:
+            return ctypes.c_float
+        elif self.type == CommandReturnType.BYTES:
+            # NOTE: functions that returns bytes should return int error codes.
+            # If code is 0 the bytes were successfully saved into the file buffer
+            return ctypes.c_int
+        elif self.type == CommandReturnType.STRING:
+            return ctypes.c_char_p
+        elif self.type == CommandReturnType.VOID:
+            return None
