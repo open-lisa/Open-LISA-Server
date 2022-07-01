7dd1efcbbfc5dbc607b66634f8634e392934c86c
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-07-01 20:54:43 -0300

    implement get image test

--- open_lisa/tests/test_integration.py
+++ open_lisa/tests/test_integration.py
@@ -5,6 +5,7 @@ import Open_LISA_SDK
 from open_lisa.api.api import OpenLISA
 from open_lisa.config.config import load_config
 from open_lisa.protocol.rs232configuration import RS232Configuration
+from open_lisa.repositories.commands_repository import CommandsRepository
 from time import sleep
 from threading import Thread
 
@@ -13,6 +14,7 @@ LOCALHOST = "127.0.0.1"
 SERVER_PORT = 8080
 COMMANDS_JSON_PATH = "{}/tektronix_test_commands.json".format(
     os.path.dirname(__file__))
+MOCK_IMAGE_PATH = "data_test/clibs/mock_img.jpg"
 
 
 def start_server():
@@ -60,6 +62,12 @@ def test_get_instrument_commands():
     sdk.disconnect()
 
 
-# def test_get_image_from_mock_camera():
-#     pass
-#     # Implement after refactor with GUASONESCO image
+def test_get_image_from_mock_camera():
+    sdk = Open_LISA_SDK.SDK(log_level="ERROR")
+    sdk.connect_through_TCP(host=LOCALHOST, port=SERVER_PORT)
+    instruments = sdk.list_instruments()
+    mocked_camera = instruments[1]
+    image_bytes = mocked_camera.send("get_image", "bytes")
+
+    with open(MOCK_IMAGE_PATH, "rb") as f:
+        assert image_bytes == f.read()
