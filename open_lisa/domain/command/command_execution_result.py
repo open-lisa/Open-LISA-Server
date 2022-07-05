60082731abc28c2e86f49ee786e37515bef2e3ed
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-07-05 20:19:59 -0300

    implement CommandExecutionResult with tests

---
+++ open_lisa/domain/command/command_execution_result.py
@@ -0,0 +1,36 @@
+
+import base64
+from datetime import timezone, datetime
+from open_lisa.domain.command.command_return import CommandReturnType
+
+
+class CommandExecutionResult():
+    def __init__(self, type=CommandReturnType.VOID, raw_value=None):
+        assert isinstance(type, CommandReturnType)
+
+        # UTC timestamp
+        # Src: https://www.geeksforgeeks.org/get-utc-timestamp-in-python/#:~:text=Getting%20the%20UTC%20timestamp,convert%20our%20datetime%20to%20UTC.
+        self.timestamp = datetime.now(
+            timezone.utc).replace(tzinfo=timezone.utc).timestamp()
+        self.type = type
+
+        if type == CommandReturnType.VOID:
+            self.value = None
+        elif type == CommandReturnType.STRING:
+            self.value = str(raw_value)
+        elif type == CommandReturnType.INT:
+            self.value = int(raw_value)
+        elif type == CommandReturnType.FLOAT:
+            self.value = float(raw_value)
+        elif type == CommandReturnType.BYTES:
+            # bytes are encoded in base64 in order to
+            # can be represented in json format.
+            # Ref: https://stackoverflow.com/questions/27837346/how-to-include-pictures-bytes-to-a-json-with-python-encoding-issue
+            self.value = base64.b64encode(raw_value)
+
+    def to_dict(self):
+        return {
+            "type": str(self.type),
+            "value": self.value,
+            "timestamp": self.timestamp
+        }
---
+++ open_lisa/domain/tests/command/test_command_execution_result.py
@@ -0,0 +1,48 @@
+import base64
+import pytest
+from open_lisa.domain.command.command_execution_result import CommandExecutionResult
+from open_lisa.domain.command.command_return import CommandReturnType
+
+MOCK_IMAGE_PATH = "data_test/clibs/mock_img.jpg"
+
+
+def test_command_execution_result_should_be_init_with_CommandReturnType():
+    with pytest.raises(Exception):
+        CommandExecutionResult(type="invalid type")
+
+
+def test_command_execution_result_is_created_with_a_timestamp():
+    result = CommandExecutionResult(type=CommandReturnType.VOID)
+    assert isinstance(result.timestamp, float)
+
+
+def test_command_execution_result_with_void_type():
+    result = CommandExecutionResult(type=CommandReturnType.VOID)
+    assert result.value == None
+
+
+def test_command_execution_result_with_string_type():
+    result = CommandExecutionResult(
+        type=CommandReturnType.STRING, raw_value="any")
+    assert result.value == "any"
+
+
+def test_command_execution_result_with_int_type():
+    result = CommandExecutionResult(
+        type=CommandReturnType.INT, raw_value="4")
+    assert result.value == 4
+
+
+def test_command_execution_result_with_float_type():
+    result = CommandExecutionResult(
+        type=CommandReturnType.FLOAT, raw_value="4.2")
+    assert result.value == 4.2
+
+
+def test_command_execution_result_with_bytes_type():
+    with open(MOCK_IMAGE_PATH, "rb") as f:
+        image_bytes = f.read()
+        result = CommandExecutionResult(
+            type=CommandReturnType.BYTES, raw_value=image_bytes)
+        assert result.value == base64.b64encode(image_bytes)
+        assert image_bytes == base64.b64decode(result.value)
