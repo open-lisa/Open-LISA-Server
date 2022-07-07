6683110b75e6cde9b84375ae1898bb9c9fa4bd0b
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-07-07 19:21:12 -0300

    remove clib unused code

--- c/osx/example/mult.h
+++
@@ -1,7 +0,0 @@
-#ifdef _MSC_VER
-    #define EXPORT_SYMBOL __declspec(dllexport)
-#else
-    #define EXPORT_SYMBOL
-#endif
-
-EXPORT_SYMBOL float mult(int int_param, float float_param);
--- c/win/pixelflyqe/build.sh
+++
@@ -1,2 +0,0 @@
-rm *.so *.o
-gcc -c -Wall -Werror -fpic pixelflyqe.c && gcc -shared -o libpixelflyqe.dll pixelflyqe.o
