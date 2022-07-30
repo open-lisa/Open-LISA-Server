ca4249a12592ad2a3195a939eacaba302c63a06d
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-07-30 18:40:07 -0300

    recompile without errno extern declaration

--- data_test/clibs/pixelflyqe.c
+++ data_test/clibs/pixelflyqe.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include "pixelflyqe.h"
 
-extern int errno;
-
 int init(int int_param, float float_param) {
     float some_calculation = int_param * float_param;
     printf("int: %d float %.1f, calculation  %.1f\n", int_param, float_param, some_calculation);
