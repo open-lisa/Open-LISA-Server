3a5a0623b17afccf8f54f584b31b28601e98783d
Gabriel Robles <grobles@fi.uba.ar>
2022-08-04 02:33:44 +0200

    fix demo tests

--- demo/python-sdk/4_execute_bash_command.py
+++ demo/python-sdk/4_execute_bash_command.py
@@ -1,6 +1,6 @@
 import Open_LISA_SDK
 
-MATLAB_SCRIPT = "'C:\\Users\\FIUBA\\PycharmProjects\\Open-LISA-Server\\processing_example.m'"
+MATLAB_SCRIPT = "'C:\\Users\\FIUBA\\PycharmProjects\\Open-LISA-Server\\data_test\\user\\sandbox\\processing_example.m'"
 MATLAB_COMMAND = "matlab -nodisplay -nosplash -nodesktop -r \"run {}; exit;\"".format(MATLAB_SCRIPT)
 
 
@@ -8,6 +8,8 @@ def main():
     sdk = Open_LISA_SDK.SDK(log_level="DEBUG")
     sdk.connect_through_TCP("localhost", 8080)
 
+    sdk.send_file("./image_input.bmp", "sandbox/image_input.bmp")
+    sdk.send_file("./processing_example.m", "sandbox/processing_example.m")
     return_code, stdout, stderr = sdk.execute_bash_command(MATLAB_COMMAND, True, True)
 
     print(return_code)
