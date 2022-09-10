ff7f7d4224a594da8997ca9aefc303d9c6b12825
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-09-10 11:29:22 -0300

    refactor list_directory test in order to add empty folder

--- open_lisa/domain/tests/filesystem/test_filemanager.py
+++ open_lisa/domain/tests/filesystem/test_filemanager.py
@@ -16,6 +16,7 @@ with open(__file__, "rb") as f:
 def test_list_directory_mockdir_to_be_tested_for_list_should_return_a_list_representing_the_directory():
     # NOTE: expected_directory should have the same order that is returned, otherwise the test would fail
     # the order is: first directories (alphabetically) and then files (alphabetically)
+    empty_directory_name = "empty"
     expected_directory = [
         {
             "name": "1",
@@ -50,6 +51,11 @@ def test_list_directory_mockdir_to_be_tested_for_list_should_return_a_list_repre
                     "type": "file"
                 },
             ]
+        },
+        {
+            "name": empty_directory_name,
+            "type": "directory",
+            "children": []
         }
     ]
 
@@ -58,6 +64,12 @@ def test_list_directory_mockdir_to_be_tested_for_list_should_return_a_list_repre
     curr_file_path = os.path.dirname(__file__)
     mock_directory = os.path.join(
         curr_file_path, "mockdir_to_be_tested_for_list")
+
+    # create empty directory if needed
+    empty_directory = os.path.join(
+        curr_file_path, "mockdir_to_be_tested_for_list", empty_directory_name)
+    if not os.path.isdir(empty_directory):
+        os.makedirs(empty_directory)
     result = fm.list_directory_recursively(mock_directory)
     assert result == expected_directory
 
