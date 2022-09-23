1eb78d62921358e96547b15da65e20a6ee7a4a39
Gabriel Robles <grobles@fi.uba.ar>
2022-09-23 04:39:39 +0200

    wrapper on pdc_detect_device function

--- data_dev/clibs/PHOTRON_CAMERA/pdclib_wrapper.c
+++ data_dev/clibs/PHOTRON_CAMERA/pdclib_wrapper.c
@@ -29,6 +29,15 @@ unsigned long PDC_DetectDevice(
 */
 typedef UINT (CALLBACK* PDC_DETECT_DEVICE_FUNCTION_DLL)(UINT, UINT*, UINT, UINT, PDC_DETECT_NUM_INFO*, UINT*);
 
+/*
+unsigned long PDC_OpenDevice(
+    PPDC_DETECT_INFO pDetectInfo
+    unsigned long *pDeviceNo
+    unsigned long *pErrorCode
+)
+*/
+typedef UINT (CALLBACK* PDC_OPEN_DEVICE_FUNCTION_DLL)(PDC_DETECT_INFO*, UINT*, UINT*);
+
 FILE* open_tmp_file_buffer(const char * tmp_file_buffer) {
     FILE* output_file = fopen(tmp_file_buffer, "wb");
     if (output_file == NULL) {
@@ -153,6 +162,60 @@ int pdc_detect_device(UINT ip, const char * tmp_file_buffer) {
     fwrite(&m_ndevice_code, sizeof(UINT), 1, output_file);
     fwrite(&m_ntmp_device_no, sizeof(UINT), 1, output_file);
     fwrite(&m_ninterface_code, sizeof(UINT), 1, output_file);
+
+    return PDC_WRAPPER_SUCCEEDED;
+}
+
+/*
+    tmp_file_buffer:
+        success case (PDC_WRAPPER_SUCCEEDED):
+            4 bytes for function return_value
+            if return_value is success:
+                4 bytes for error_code
+                4 bytes for p_device_no
+        error case (PDC_WRAPPER_FAILED):
+            string with error message
+*/
+int pdc_open_device(UINT m_ndevice_code, UINT m_ntmp_device_no, UINT m_ninterface_code, const char * tmp_file_buffer) { 
+    HINSTANCE libHandle;
+
+    PDC_OPEN_DEVICE_FUNCTION_DLL pdc_open_device_function;
+    UINT p_device_no, error_code, return_value;
+    PDC_DETECT_INFO pdc_detect_info;
+    pdc_detect_info.m_nDeviceCode = m_ndevice_code;
+    pdc_detect_info.m_nTmpDeviceNo = m_ntmp_device_no;
+    pdc_detect_info.m_nInterfaceCode = m_ninterface_code;
+
+    FILE* output_file = open_tmp_file_buffer(tmp_file_buffer);
+    if (output_file == NULL) {
+        return PDC_WRAPPER_FAILED;
+    }
+
+    libHandle = LoadLibrary("PDCLIB.dll");
+    if (libHandle == NULL) {
+        const char * message = "error loading library PDCLIB.dll";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    pdc_open_device_function = (PDC_OPEN_DEVICE_FUNCTION_DLL) GetProcAddress(libHandle, "PDC_OpenDevice");
+    if (pdc_open_device_function == NULL) {
+        const char * message = "GetProcAddress failed loading PDC_OpenDevice function";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    return_value = pdc_open_device_function(&pdc_detect_info, &p_device_no, &error_code);
+
+    fwrite(&return_value, sizeof(UINT), 1, output_file);
+    if (return_value == PDC_FAILED) {
+        return PDC_WRAPPER_SUCCEEDED;
+    }
+
+    fwrite(&error_code, sizeof(UINT), 1, output_file);
+    fwrite(&p_device_no, sizeof(UINT), 1, output_file);
 
     return PDC_WRAPPER_SUCCEEDED;
 }
