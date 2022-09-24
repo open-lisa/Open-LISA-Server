4e52db2046a837c311ebb9160a6f4309b7d28bde
Gabriel Robles <grobles@fi.uba.ar>
2022-09-24 23:40:19 +0200

    wrap new function

--- data/clibs/PHOTRON_CAMERA/pdclib_wrapper.c
+++ data/clibs/PHOTRON_CAMERA/pdclib_wrapper.c
@@ -116,6 +116,17 @@ unsigned long PDC_GetColorType(
 */
 typedef UINT (CALLBACK* PDC_GET_COLOR_TYPE_FUNCTION_DLL)(UINT, UINT, char*, UINT*);
 
+/*
+unsigned long PDC_GetRecordRate(
+    unsigned long nDeviceNo
+    unsigned long nChildNo
+    unsigned long *pRate
+    unsigned long *pErrorCode
+)
+*/
+typedef UINT (CALLBACK* PDC_GET_RECORD_RATE_FUNCTION_DLL)(UINT, UINT, UINT*, UINT*);
+
+
 FILE* open_tmp_file_buffer(const char * tmp_file_buffer) {
     FILE* output_file = fopen(tmp_file_buffer, "wb");
     if (output_file == NULL) {
@@ -642,6 +653,53 @@ int pdc_get_color_type(UINT n_device_no, UINT n_child_no, const char * tmp_file_
     fwrite(&return_value, sizeof(UINT), 1, output_file);
     fwrite(&error_code, sizeof(UINT), 1, output_file);
     fwrite(&color_type, sizeof(char), 1, output_file);
+    fclose(output_file);
+
+    return PDC_WRAPPER_SUCCEEDED;
+}
+
+/*
+    tmp_file_buffer:
+        success case (PDC_WRAPPER_SUCCEEDED):
+            4 bytes for return_value
+            4 bytes for error_code
+            4 bytes for record rate
+        error case (PDC_WRAPPER_FAILED):
+            string with error message
+*/
+int pdc_get_record_rate(UINT n_device_no, UINT n_child_no, const char * tmp_file_buffer) {
+    HINSTANCE libHandle;
+
+    PDC_GET_RECORD_RATE_FUNCTION_DLL pdc_get_record_rate_function_dll;
+    UINT error_code, return_value;
+    UINT record_rate;
+
+    FILE* output_file = open_tmp_file_buffer(tmp_file_buffer);
+    if (output_file == NULL) {
+        return PDC_WRAPPER_FAILED;
+    }
+
+    libHandle = LoadLibrary("PDCLIB.dll");
+    if (libHandle == NULL) {
+        const char * message = "error loading library PDCLIB.dll";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    pdc_get_record_rate_function_dll = (PDC_GET_RECORD_RATE_FUNCTION_DLL) GetProcAddress(libHandle, "PDC_GetRecordRate");
+    if (pdc_get_record_rate_function_dll == NULL) {
+        const char * message = "GetProcAddress failed loading PDC_GetRecordRate function";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    return_value = pdc_get_record_rate_function_dll(n_device_no, n_child_no, &record_rate, &error_code);
+
+    fwrite(&return_value, sizeof(UINT), 1, output_file);
+    fwrite(&error_code, sizeof(UINT), 1, output_file);
+    fwrite(&record_rate, sizeof(UINT), 1, output_file);
     fclose(output_file);
 
     return PDC_WRAPPER_SUCCEEDED;
