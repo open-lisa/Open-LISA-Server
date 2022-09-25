bfdbcaa7fec28a354e7a338f38f48a1463dfd0d3
Gabriel Robles <grobles@fi.uba.ar>
2022-09-26 01:05:20 +0200

    add function wrapper

--- data/clibs/PHOTRON_CAMERA/pdclib_wrapper.c
+++ data/clibs/PHOTRON_CAMERA/pdclib_wrapper.c
@@ -222,6 +222,16 @@ unsigned long PDC_GetLUTParams(
 */
 typedef UINT (CALLBACK* PDC_GET_LUT_PARAMS_FUNCTION_DLL)(UINT, UINT, UINT, PPDC_LUT_PARAMS*, UINT*);
 
+/*
+unsigned long PDC_GetShadingMode(
+    unsigned long nDeviceNo
+    unsigned long nChildNo
+    unsigned long *pMode
+    unsigned long *pErrorCode
+)
+*/
+typedef UINT (CALLBACK* PDC_GET_SHADING_MODE_FUNCTION_DLL)(UINT, UINT, UINT*, UINT*);
+
 
 FILE* open_tmp_file_buffer(const char * tmp_file_buffer) {
     FILE* output_file = fopen(tmp_file_buffer, "wb");
@@ -1272,6 +1282,53 @@ int pdc_get_lut_params(UINT n_device_no, UINT n_child_no, UINT lut_mode, const c
     fwrite(&(pdc_lut_params->m_nPosiNegaB), sizeof(UCHAR), 1, output_file);
     fwrite(&(pdc_lut_params->m_nFreeMode), sizeof(UCHAR), 1, output_file);
 
+    fclose(output_file);
+
+    return PDC_WRAPPER_SUCCEEDED;
+}
+
+/*
+    tmp_file_buffer:
+        success case (PDC_WRAPPER_SUCCEEDED):
+            4 bytes for return_value
+            4 bytes for error_code
+            4 bytes for mode
+        error case (PDC_WRAPPER_FAILED):
+            string with error message
+*/
+int pdc_get_shading_mode(UINT n_device_no, UINT n_child_no, const char * tmp_file_buffer) {
+    HINSTANCE libHandle;
+
+    PDC_GET_SHADING_MODE_FUNCTION_DLL pdc_get_shading_mode_function_dll;
+    UINT error_code, return_value;
+    UINT mode;
+
+    FILE* output_file = open_tmp_file_buffer(tmp_file_buffer);
+    if (output_file == NULL) {
+        return PDC_WRAPPER_FAILED;
+    }
+
+    libHandle = LoadLibrary("PDCLIB.dll");
+    if (libHandle == NULL) {
+        const char * message = "error loading library PDCLIB.dll";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    pdc_get_shading_mode_function_dll = (PDC_GET_SHADING_MODE_FUNCTION_DLL) GetProcAddress(libHandle, "PDC_GetShadingMode");
+    if (pdc_get_shading_mode_function_dll == NULL) {
+        const char * message = "GetProcAddress failed loading PDC_GetShadingMode function";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    return_value = pdc_get_shading_mode_function_dll(n_device_no, n_child_no, &mode, &error_code);
+
+    fwrite(&return_value, sizeof(UINT), 1, output_file);
+    fwrite(&error_code, sizeof(UINT), 1, output_file);
+    fwrite(&mode, sizeof(UINT), 1, output_file);
     fclose(output_file);
 
     return PDC_WRAPPER_SUCCEEDED;
