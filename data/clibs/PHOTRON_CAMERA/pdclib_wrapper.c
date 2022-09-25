6c8b0de8a53da1758d103aeb79e1bd6a134e936d
Gabriel Robles <grobles@fi.uba.ar>
2022-09-25 19:38:43 +0200

    add new function wrap

--- data/clibs/PHOTRON_CAMERA/pdclib_wrapper.c
+++ data/clibs/PHOTRON_CAMERA/pdclib_wrapper.c
@@ -137,6 +137,18 @@ unsigned long PDC_GetMaxResolution(
 */
 typedef UINT (CALLBACK* PDC_GET_MAX_RESOLUTION_FUNCTION_DLL)(UINT, UINT, UINT*, UINT*, UINT*);
 
+/*
+unsigned long PDC_GetResolution(
+    unsigned long nDeviceNo
+    unsigned long nChildNo
+    unsigned long *pWidth
+    unsigned long *pHeight
+    unsigned long *pErrorCode
+)
+*/
+typedef UINT (CALLBACK* PDC_GET_RESOLUTION_FUNCTION_DLL)(UINT, UINT, UINT*, UINT*, UINT*);
+
+
 FILE* open_tmp_file_buffer(const char * tmp_file_buffer) {
     FILE* output_file = fopen(tmp_file_buffer, "wb");
     if (output_file == NULL) {
@@ -754,6 +766,55 @@ int pdc_get_max_resolution(UINT n_device_no, UINT n_child_no, const char * tmp_f
     }
 
     return_value = pdc_get_max_resolution_function_dll(n_device_no, n_child_no, &width, &height, &error_code);
+
+    fwrite(&return_value, sizeof(UINT), 1, output_file);
+    fwrite(&error_code, sizeof(UINT), 1, output_file);
+    fwrite(&width, sizeof(UINT), 1, output_file);
+    fwrite(&height, sizeof(UINT), 1, output_file);
+    fclose(output_file);
+
+    return PDC_WRAPPER_SUCCEEDED;
+}
+
+/*
+    tmp_file_buffer:
+        success case (PDC_WRAPPER_SUCCEEDED):
+            4 bytes for return_value
+            4 bytes for error_code
+            4 bytes for width
+            4 bytes for height
+        error case (PDC_WRAPPER_FAILED):
+            string with error message
+*/
+int pdc_get_resolution(UINT n_device_no, UINT n_child_no, const char * tmp_file_buffer) {
+    HINSTANCE libHandle;
+
+    PDC_GET_RESOLUTION_FUNCTION_DLL pdc_get_resolution_function_dll;
+    UINT error_code, return_value;
+    UINT width, height;
+
+    FILE* output_file = open_tmp_file_buffer(tmp_file_buffer);
+    if (output_file == NULL) {
+        return PDC_WRAPPER_FAILED;
+    }
+
+    libHandle = LoadLibrary("PDCLIB.dll");
+    if (libHandle == NULL) {
+        const char * message = "error loading library PDCLIB.dll";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    pdc_get_resolution_function_dll = (PDC_GET_MAX_RESOLUTION_FUNCTION_DLL) GetProcAddress(libHandle, "PDC_GetResolution");
+    if (pdc_get_resolution_function_dll == NULL) {
+        const char * message = "GetProcAddress failed loading PDC_GetResolution function";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    return_value = pdc_get_resolution_function_dll(n_device_no, n_child_no, &width, &height, &error_code);
 
     fwrite(&return_value, sizeof(UINT), 1, output_file);
     fwrite(&error_code, sizeof(UINT), 1, output_file);
