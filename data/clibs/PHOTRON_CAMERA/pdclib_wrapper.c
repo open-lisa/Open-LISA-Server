0c6d0203ec2b8bff16382f2eb79f9c131b89633b
Gabriel Robles <grobles@fi.uba.ar>
2022-09-26 01:03:09 +0200

    wrap complex function

--- data/clibs/PHOTRON_CAMERA/pdclib_wrapper.c
+++ data/clibs/PHOTRON_CAMERA/pdclib_wrapper.c
@@ -211,6 +211,17 @@ unsigned long PDC_GetLUTMode(
 */
 typedef UINT (CALLBACK* PDC_GET_LUT_MODE_FUNCTION_DLL)(UINT, UINT, UINT*, UINT*);
 
+/*
+unsigned long PDC_GetLUTParams(
+    unsigned long nDeviceNo
+    unsigned long nChildNo
+    unsigned long nLUTMode
+    PPDC_LUT_PARAMS pParams
+    unsigned long *pErrorCode
+)
+*/
+typedef UINT (CALLBACK* PDC_GET_LUT_PARAMS_FUNCTION_DLL)(UINT, UINT, UINT, PPDC_LUT_PARAMS*, UINT*);
+
 
 FILE* open_tmp_file_buffer(const char * tmp_file_buffer) {
     FILE* output_file = fopen(tmp_file_buffer, "wb");
@@ -312,6 +323,7 @@ int pdc_detect_device(UINT ip, const char * tmp_file_buffer) {
 
     fwrite(&return_value, sizeof(UINT), 1, output_file);
     if (return_value == PDC_FAILED) {
+        fclose(output_file);
         return PDC_WRAPPER_SUCCEEDED;
     }
 
@@ -1173,6 +1185,93 @@ int pdc_get_lut_mode(UINT n_device_no, UINT n_child_no, const char * tmp_file_bu
     fwrite(&return_value, sizeof(UINT), 1, output_file);
     fwrite(&error_code, sizeof(UINT), 1, output_file);
     fwrite(&mode, sizeof(UINT), 1, output_file);
+    fclose(output_file);
+
+    return PDC_WRAPPER_SUCCEEDED;
+}
+
+/*
+    tmp_file_buffer:
+        success case (PDC_WRAPPER_SUCCEEDED):
+            4 bytes for function return_value
+            4 bytes for error code
+            if return_value is success:
+                4 bytes for brightness R
+                4 bytes for brightness G
+                4 bytes for brightness B
+                4 bytes for contrast R
+                4 bytes for contrast G
+                4 bytes for contrast B
+                8 bytes for gain R
+                8 bytes for gain G
+                8 bytes for gain B
+                8 bytes for gamma R
+                8 bytes for gamma G
+                8 bytes for gamma B
+                1 byte for posi nega R
+                1 byte for posi nega G
+                1 byte for posi nega B
+                1 byte for free mode
+        error case (PDC_WRAPPER_FAILED):
+            string with error message
+*/
+int pdc_get_lut_params(UINT n_device_no, UINT n_child_no, UINT lut_mode, const char * tmp_file_buffer) {
+    HINSTANCE libHandle;
+
+    PDC_GET_LUT_PARAMS_FUNCTION_DLL pdc_get_lut_params_function_dll;
+
+    UINT error_code, return_value;
+    PPDC_LUT_PARAMS pdc_lut_params;
+
+    FILE* output_file = open_tmp_file_buffer(tmp_file_buffer);
+    if (output_file == NULL) {
+        return PDC_WRAPPER_FAILED;
+    }
+
+    libHandle = LoadLibrary("PDCLIB.dll");
+    if (libHandle == NULL) {
+        const char * message = "error loading library PDCLIB.dll";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    pdc_get_lut_params_function_dll = (PDC_GET_LUT_PARAMS_FUNCTION_DLL) GetProcAddress(libHandle, "PDC_GetLUTParams");
+    if (pdc_get_lut_params_function_dll == NULL) {
+        const char * message = "GetProcAddress failed loading PDC_GetLUTParams function";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    return_value = pdc_get_lut_params_function_dll(n_device_no, n_child_no, lut_mode, &pdc_lut_params, &error_code);
+
+    fwrite(&return_value, sizeof(UINT), 1, output_file);
+    fwrite(&error_code, sizeof(UINT), 1, output_file);
+    if (return_value == PDC_FAILED) {
+        fclose(output_file);
+        return PDC_WRAPPER_SUCCEEDED;
+    }
+
+    fwrite(&(pdc_lut_params->m_nBrightnessR), sizeof(INT), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nBrightnessG), sizeof(INT), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nBrightnessB), sizeof(INT), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nContrastR), sizeof(INT), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nContrastG), sizeof(INT), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nContrastB), sizeof(INT), 1, output_file);
+
+    fwrite(&(pdc_lut_params->m_nGainR), sizeof(DOUBLE), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nGainG), sizeof(DOUBLE), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nGainB), sizeof(DOUBLE), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nGammaR), sizeof(DOUBLE), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nGammaG), sizeof(DOUBLE), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nGammaB), sizeof(DOUBLE), 1, output_file);
+
+    fwrite(&(pdc_lut_params->m_nPosiNegaR), sizeof(UCHAR), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nPosiNegaG), sizeof(UCHAR), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nPosiNegaB), sizeof(UCHAR), 1, output_file);
+    fwrite(&(pdc_lut_params->m_nFreeMode), sizeof(UCHAR), 1, output_file);
+
     fclose(output_file);
 
     return PDC_WRAPPER_SUCCEEDED;
