7e1af3976b651a2db2b3fbc8a42df4f15f10098c
Gabriel Robles <grobles@fi.uba.ar>
2022-09-26 02:45:42 +0200

    add file mraw function wraps

--- data/clibs/PHOTRON_CAMERA/pdclib_wrapper.c
+++ data/clibs/PHOTRON_CAMERA/pdclib_wrapper.c
@@ -478,6 +478,37 @@ unsigned long PDC_AVIFileSaveClose(
 */
 typedef UINT (CALLBACK* PDC_AVI_FILE_SAVE_CLOSE_FUNCTION_DLL)(UINT, UINT, UINT*);
 
+/*
+unsigned long PDC_MRAWFileSaveOpen(
+    unsigned long   nDeviceNo
+    unsigned long   nChildNo
+    LPCTSTR         lpszFileName
+    unsigned long   nMRawBitDepth
+    long            nMaxFrameNum
+    unsigned long   *pErrorCode
+)
+*/
+typedef UINT (CALLBACK* PDC_MRAW_FILE_SAVE_OPEN_FUNCTION_DLL)(UINT, UINT, const CHAR*, UINT, UINT, UINT*);
+
+/*
+unsigned long PDC_MRAWFileSave(
+    unsigned long  nDeviceNo
+    unsigned long  nChildNo
+    long           nFrameNo
+    unsigned long  *pErrorCode
+)
+*/
+typedef UINT (CALLBACK* PDC_MRAW_FILE_SAVE_FUNCTION_DLL)(UINT, UINT, UINT, UINT*);
+
+/*
+unsigned long PDC_MRAWFileSaveClose(
+    unsigned long  nDeviceNo
+    unsigned long  nChildNo
+    unsigned long  *pErrorCode
+)
+*/
+typedef UINT (CALLBACK* PDC_MRAW_FILE_SAVE_CLOSE_FUNCTION_DLL)(UINT, UINT, UINT*);
+
 
 FILE* open_tmp_file_buffer(const char * tmp_file_buffer) {
     FILE* output_file = fopen(tmp_file_buffer, "wb");
@@ -2699,6 +2730,138 @@ int pdc_avi_file_save_close(UINT n_device_no, UINT n_child_no, const char * tmp_
     }
 
     return_value = pdc_avi_file_save_close_function_dll(n_device_no, n_child_no, &error_code);
+
+    fwrite(&return_value, sizeof(UINT), 1, output_file);
+    fwrite(&error_code, sizeof(UINT), 1, output_file);
+    fclose(output_file);
+
+    return PDC_WRAPPER_SUCCEEDED;
+}
+
+/*
+    tmp_file_buffer:
+        success case (PDC_WRAPPER_SUCCEEDED):
+            4 bytes for return_value
+            4 bytes for error_code
+        error case (PDC_WRAPPER_FAILED):
+            string with error message
+*/
+int pdc_mraw_file_save_open(UINT n_device_no, UINT n_child_no, const char * file_name, UINT raw_bit_depth, UINT max_frame_num, const char * tmp_file_buffer) {
+    HINSTANCE libHandle;
+
+    PDC_MRAW_FILE_SAVE_OPEN_FUNCTION_DLL pdc_mraw_file_save_open_function_dll;
+    UINT error_code, return_value;
+
+    FILE* output_file = open_tmp_file_buffer(tmp_file_buffer);
+    if (output_file == NULL) {
+        return PDC_WRAPPER_FAILED;
+    }
+
+    libHandle = LoadLibrary("PDCLIB.dll");
+    if (libHandle == NULL) {
+        const char * message = "error loading library PDCLIB.dll";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    pdc_mraw_file_save_open_function_dll = (PDC_MRAW_FILE_SAVE_OPEN_FUNCTION_DLL) GetProcAddress(libHandle, "PDC_MRAWFileSaveOpenA");
+    if (pdc_mraw_file_save_open_function_dll == NULL) {
+        const char * message = "GetProcAddress failed loading PDC_MRAWFileSaveOpenA function";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    return_value = pdc_mraw_file_save_open_function_dll(n_device_no, n_child_no, file_name, raw_bit_depth, max_frame_num, &error_code);
+
+    fwrite(&return_value, sizeof(UINT), 1, output_file);
+    fwrite(&error_code, sizeof(UINT), 1, output_file);
+    fclose(output_file);
+
+    return PDC_WRAPPER_SUCCEEDED;
+}
+
+/*
+    tmp_file_buffer:
+        success case (PDC_WRAPPER_SUCCEEDED):
+            4 bytes for return_value
+            4 bytes for error_code
+        error case (PDC_WRAPPER_FAILED):
+            string with error message
+*/
+int pdc_mraw_file_save(UINT n_device_no, UINT n_child_no, UINT n_frame_no, const char * tmp_file_buffer) {
+    HINSTANCE libHandle;
+
+    PDC_MRAW_FILE_SAVE_FUNCTION_DLL pdc_mraw_file_save_function_dll;
+    UINT error_code, return_value;
+
+    FILE* output_file = open_tmp_file_buffer(tmp_file_buffer);
+    if (output_file == NULL) {
+        return PDC_WRAPPER_FAILED;
+    }
+
+    libHandle = LoadLibrary("PDCLIB.dll");
+    if (libHandle == NULL) {
+        const char * message = "error loading library PDCLIB.dll";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    pdc_mraw_file_save_function_dll = (PDC_MRAW_FILE_SAVE_FUNCTION_DLL) GetProcAddress(libHandle, "PDC_MRAWFileSave");
+    if (pdc_mraw_file_save_function_dll == NULL) {
+        const char * message = "GetProcAddress failed loading PDC_MRAWFileSave function";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    return_value = pdc_mraw_file_save_function_dll(n_device_no, n_child_no, n_frame_no, &error_code);
+
+    fwrite(&return_value, sizeof(UINT), 1, output_file);
+    fwrite(&error_code, sizeof(UINT), 1, output_file);
+    fclose(output_file);
+
+    return PDC_WRAPPER_SUCCEEDED;
+}
+
+/*
+    tmp_file_buffer:
+        success case (PDC_WRAPPER_SUCCEEDED):
+            4 bytes for return_value
+            4 bytes for error_code
+        error case (PDC_WRAPPER_FAILED):
+            string with error message
+*/
+int pdc_mraw_file_save_close(UINT n_device_no, UINT n_child_no, const char * tmp_file_buffer) {
+    HINSTANCE libHandle;
+
+    PDC_MRAW_FILE_SAVE_CLOSE_FUNCTION_DLL pdc_mraw_file_save_close_function_dll;
+    UINT error_code, return_value;
+
+    FILE* output_file = open_tmp_file_buffer(tmp_file_buffer);
+    if (output_file == NULL) {
+        return PDC_WRAPPER_FAILED;
+    }
+
+    libHandle = LoadLibrary("PDCLIB.dll");
+    if (libHandle == NULL) {
+        const char * message = "error loading library PDCLIB.dll";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    pdc_mraw_file_save_close_function_dll = (PDC_MRAW_FILE_SAVE_CLOSE_FUNCTION_DLL) GetProcAddress(libHandle, "PDC_MRAWFileSaveClose");
+    if (pdc_mraw_file_save_close_function_dll == NULL) {
+        const char * message = "GetProcAddress failed loading PDC_MRAWFileSaveClose function";
+        fwrite(message, sizeof(char), strlen(message), output_file);
+        fclose(output_file);
+        return PDC_WRAPPER_FAILED;
+    }
+
+    return_value = pdc_mraw_file_save_close_function_dll(n_device_no, n_child_no, &error_code);
 
     fwrite(&return_value, sizeof(UINT), 1, output_file);
     fwrite(&error_code, sizeof(UINT), 1, output_file);
