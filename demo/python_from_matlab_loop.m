0aa39c8d1d988404c85c9966737617e502efe757
Gabriel Robles <grobles@fi.uba.ar>
2022-10-01 03:33:15 +0200

    matlab script camera wip

--- demo/python_from_matlab_loop.m
+++ demo/python_from_matlab_loop.m
@@ -1,10 +1,11 @@
 function python_from_matlab_loop()
 % Define the Python interpreter
 clc
-pcPythonExe = 'C:\Users\LFD\anaconda3\python.exe';
+pcPythonExe = 'C:\Users\gabro\AppData\Local\Programs\Python\Python39\python.exe';
 % [ver, exec, loaded]	= pyversion(pcPythonExe);
 pyversion
 py.print("Connecting with server")
+%%
 sdk = py.Open_LISA_SDK.SDK(pyargs('log_level',"DEBUG", 'default_response_format',"JSON"));
 %%
 % Waveform
@@ -23,12 +24,13 @@ try
     fg_id = '1';
     ps_id = '2';
     os_id = '3';
-    cam_id = '4';
+    cam_id = '330355862202759620';
 catch
     disp()
     disp(ME.message)
     sdk.disconnect()
 end
+%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Power Supply
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -127,123 +129,137 @@ for n = os.settings.chs.on
     sdk.send_command(pyargs('instrument_id', os_id, 'command_invocation', fprintf('set_ch_scale %d %s', n, os.settings.chs.scale{n})));
     sdk.send_command(pyargs('instrument_id', os_id, 'command_invocation', fprintf('set_ch_disp %d %s', n, os.settings.chs.offset(n))));
 end
+%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Camera
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % load constant values from PDCVALUE.h library
-LIBDIR = fullfile('.', 'data', 'clibs', 'PHOTRON_CAMERA', 'Include');
+LIBDIR = fullfile('..', 'data', 'clibs', 'PHOTRON_CAMERA', 'Include');
 if not(libisloaded('PDCVALUE'))
-    loadlibrary('PDCVALUE', 'PDCVALUE.h', 'alias', 'lib', 'includepath', LIBDIR)
+    loadlibrary('PDCVALUE', fullfile(LIBDIR,'PDCVALUE.h'), 'alias', 'lib')
 end
+%%
 IP_HEXA = "C0A8000A";
-IP_INT = int(IP_HEXA, 16);
+IP_INT = uint32(hex2dec(IP_HEXA));
+%%
 % Init Library
 cm = sprintf('init');
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
-assert(logical(ret), sprintf('Error in %s', cm));
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
+% assert(logical(ret), sprintf('Error in %s', cm));
+%%
 % Detect Camera
 cm = sprintf('detect_device %d',IP_INT);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
+%%
 assert(logical(ret), sprintf('Error in %s', 'detect_device'));
+%%
 DEVICE_CODE = out(2);
 temp_DEVICE_NO = out(3);
 DEVICE_INTERFACE = out(4);
+%%
 % Open Device
 cm = sprintf('open_device %d %d %d',DEVICE_CODE, temp_DEVICE_NO, DEVICE_INTERFACE);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'open_device'));
+%%
 DEVICE_CODE = out(2);
 DEVICE_NO = out(3);
-% Child No
+%% Child No
 cm = sprintf('get_exist_child_device_list %d',DEVICE_NO);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', cm));
 CHILD_NO = out(2);
-% Set Record Rate
+%% Set Record Rate
 RECORD_RATE = 4000;
 cm = sprintf('set_record_rate %d %d %d', DEVICE_NO, CHILD_NO, RECORD_RATE);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'set_record_rate'));
-% Set Shutter Speed
+%% Set Shutter Speed
 SHUTTER_SPEED = 8000;
 cm = sprintf('set_shutter_speed_fps %d %d %d', DEVICE_NO, CHILD_NO, SHUTTER_SPEED);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'set_shutter_speed_fps'));
-% Set Resolution
+%% Set Resolution
 WIDTH = 1280;   HEIGHT = 512;
 cm = sprintf('set_resolution %d %d %d %d', DEVICE_NO, CHILD_NO, WIDTH, HEIGHT);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'set_resolution'));
-% Set Trigger
+%% Set Trigger
+PDC_TRIGGER_START = 0;
 cm = sprintf('set_trigger_mode %d %d %d %d %d', DEVICE_NO, PDC_TRIGGER_START, 0, 0, 0);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'set_trigger_mode'));
-% Set LUT
+%% Set LUT
+PDC_LUT_DEFAULT4 = 4;
 cm = sprintf('set_lut_mode %d %d %d', DEVICE_NO, CHILD_NO, PDC_LUT_DEFAULT4);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'set_lut_mode'));
-% Set Output ports
+%% Set Output ports
+PDC_EXT_OUT_TRIGGER_POSI = 5;
 cm = sprintf('set_external_out_mode %d %d %d',DEVICE_NO, 1, PDC_EXT_OUT_TRIGGER_POSI);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'set_external_out_mode port 1'));
-%
+%%
+PDC_EXT_OUT_RECORD_POSI = 3;
 cm = sprintf('set_external_out_mode %d %d %d', DEVICE_NO, 2, PDC_EXT_OUT_RECORD_POSI);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'set_external_out_mode port 2'));
-% Reset status to LIVE
+%% Reset status to LIVE
+PDC_STATUS_LIVE = 0;
 cm = sprintf('set_status %d %d', DEVICE_NO, PDC_STATUS_LIVE);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'reset_status_live'));
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%% DATA CAPTURE %%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Set Camera Ready
-RECORDING = FALSE;
-cm_trigger = sprintf('trigger_in %d %d', DEVICE_NO, CHILD_NO);
-cm = sprintf('set_rec_ready %d %d', DEVICE_NO, CHILD_NO);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+RECORDING = false;
+cm_trigger = sprintf('trigger_in %d', DEVICE_NO);
+cm = sprintf('set_rec_ready %d', DEVICE_NO);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'set_rec_ready'));
-% FG ON
+%% FG ON
 sdk.send_command(pyargs('instrument_id', fg_id, 'command_invocation', 'output on'));
 pause(1.5)
-% Laser ON
+%% Laser ON
 sdk.send_command(pyargs('instrument_id', ps_id, 'command_invocation', 'output_ch 2 on'));
 pause(0.5)
-% Oscilloscope ON
+%% Oscilloscope ON
 sdk.send_command(pyargs('instrument_id', os_id, 'command_invocation', 'run'));
-% Camera Trigger IN
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm_trigger));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+%% Camera Trigger IN
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm_trigger));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'trigger_in'));
-RECORDING = TRUE;
-cm = sprintf('get_status %d %d',DEVICE_NO, CHILD_NO);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+%%
+RECORDING = true;
+cm = sprintf('get_status %d',DEVICE_NO);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'get_status'));
 status = out(2);
-% Let the camera recording
+%% Let the camera recording
 while status == PDC_STATUS_REC
     cm = sprintf('get_status %d %d', DEVICE_NO, CHILD_NO);
-    bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-    out = bytes_to_ints(bytes_out);  ret = out(1);
+    json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+    out = json_2_ints(json_out);  ret = out(1);
     assert(logical(ret), sprintf('Error in %s', 'get_status'));
     status = out(2);
 end
-RECORDING = FALSE;
+RECORDING = false;
 % Laser Off
 sdk.send_command(pyargs('instrument_id', ps_id, 'command_invocation', 'output_ch 2 off'));
 % FG Off
@@ -257,18 +273,19 @@ sdk.send_command(pyargs('instrument_id', os_id, 'command_invocation', 'stop'));
 %
 % Images
 % Set Playback Mode
+PDC_STATUS_PLAYBACK = 1;
 cm = sprintf('set_status %d %d', DEVICE_NO, PDC_STATUS_PLAYBACK);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'set_status playback'));
-
+%%
 fileID = 'openlisa_video';
 fileName = SaveFormatFile(sdk, cam_id, fileID, DEVICE_NO, CHILD_NO);
 
-% Set Live Status back
+%% Set Live Status back
 cm = sprintf('set_status %d %d', DEVICE_NO, PDC_STATUS_LIVE);
-bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-out = bytes_to_ints(bytes_out);  ret = out(1);
+json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+out = json_2_ints(json_out);  ret = out(1);
 assert(logical(ret), sprintf('Error in %s', 'reset_status_live'));
 
 %
@@ -281,63 +298,66 @@ end
 %% 
 %%%%%%%%%%%%%%%%
 % Functions
-%%%%%%%%%%%%%%%%
-function ints = bytes_to_ints(bytes)
-    ints = [];
-    int_size = 4;
-    for ii = 1:int_size:length(bytes)
-        int_bytes = bytes(ii:ii+int_size-1);
-        int_value = swapbytes(typecast(uint8(int_bytes), 'uint32'));
-        ints = [ints int_value];
-    end
-end
+% %%%%%%%%%%%%%%%%
+% function ints = json_2_ints(json_out)
+%     struct_out = jsondecode(char(json_out));
+%     encoded_value = struct_out.value;
+%     bytes = matlab.net.base64decode(encoded_value);
+%     ints = [];
+%     int_size = 4;
+%     for ii = 1:int_size:length(bytes)
+%         int_bytes = bytes(ii:ii+int_size-1);
+%         int_value = swapbytes(typecast(uint8(int_bytes), 'uint32'));
+%         ints = [ints int_value];
+%     end
+% end
 %
-function fileName = SaveFormatFile(sdk, cam_id, fileID, DEVICE_NO, CHILD_NO)
-    LIBDIR = fullfile('.', 'data', 'clibs', 'PHOTRON_CAMERA', 'Include');
-    if not(libisloaded('PDCVALUE'))
-        loadlibrary('PDCVALUE', 'PDCVALUE.h', 'alias', 'lib', 'includepath', LIBDIR)
-    end
-    fileName = [fileID '.mraw'];
-    file_path = fullfile(pwd, 'movies', fileName);
-    assert(fileName ~= 0, sprintf('Error in %s', 'SaveFormatFile: No FileName'));
-    % Get Mem Info
-    cm = sprintf('get_mem_frame_info %d %d',DEVICE_NO, CHILD_NO);
-    bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-    out = bytes_to_ints(bytes_out);  ret = out(1);
-    assert(logical(ret), sprintf('Error in %s', 'SaveFormatFile: get_mem_frame_info'));
-    frameInfo = out(2);
-    startNo        = frameInfo.m_nStart;
-    recordedFrames = frameInfo.m_nRecordedFrames;
-    endNo          = frameInfo.m_nEnd;
-    SAVEDFRAMES    = 300;
-    % Get Record Rate 
-    cm = sprintf('get_record_rate %d %d', DEVICE_NO, CHILD_NO);
-    bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-    out = bytes_to_ints(bytes_out);  ret = out(1);
-    assert(logical(ret), sprintf('Error in %s', 'SaveFormatFile: get_record_rate'));
-    record_rate = out(2)
-    % Open File
-    cm = sprintf('mraw_file_save_open %d %d %s %d %d', DEVICE_NO, CHILD_NO, file_path, PDC_MRAW_BITDEPTH_16, 0);
-    bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-    out = bytes_to_ints(bytes_out);  ret = out(1);
-    assert(logical(ret), sprintf('Error in %s', 'SaveFormatFile: mraw_file_save_open'));
-    % Load Images to file
-    for i=1:SAVEDFRAMES+1
-        frameNo = startNo + int32(i - 1);
-        if frameNo < 0
-            frameNo = endNo + abs(startNo) + frameNo + int32(1);
-        end    
-        cm = sprintf('mraw_file_save %d %d %d', DEVICE_NO, CHILDE_NO, frameNo);
-        bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-        out = bytes_to_ints(bytes_out);  ret = out(1);
-        if ret == PDC_FAILED
-            cm = sprintf('mraw_file_save_close %d %d', DEVICE_NO, CHILD_NO);
-            bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-            out = bytes_to_ints(bytes_out);  ret = out(1);
-            error(2,'Error in %s', 'SaveFormatFile: mraw_file_save_open')
-        end
-    end
-    cm = sprintf('mraw_file_save_close %d %d', DEVICE_NO, CHILD_NO);
-    bytes_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
-    out = bytes_to_ints(bytes_out);  ret = out(1);
-end
+% function fileName = SaveFormatFile(sdk, cam_id, fileID, DEVICE_NO, CHILD_NO)
+%     LIBDIR = fullfile('.', 'data', 'clibs', 'PHOTRON_CAMERA', 'Include');
+%     if not(libisloaded('PDCVALUE'))
+%         loadlibrary('PDCVALUE', 'PDCVALUE.h', 'alias', 'lib', 'includepath', LIBDIR)
+%     end
+%     fileName = [fileID '.mraw'];
+%     file_path = fullfile(pwd, 'movies', fileName);
+%     assert(fileName ~= 0, sprintf('Error in %s', 'SaveFormatFile: No FileName'));
+%     % Get Mem Info
+%     cm = sprintf('get_mem_frame_info %d %d',DEVICE_NO, CHILD_NO);
+%     json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+%     out = json_2_ints(json_out);  ret = out(1);
+%     assert(logical(ret), sprintf('Error in %s', 'SaveFormatFile: get_mem_frame_info'));
+%     frameInfo = out(2);
+%     startNo        = frameInfo.m_nStart;
+%     recordedFrames = frameInfo.m_nRecordedFrames;
+%     endNo          = frameInfo.m_nEnd;
+%     SAVEDFRAMES    = 300;
+%     % Get Record Rate 
+%     cm = sprintf('get_record_rate %d %d', DEVICE_NO, CHILD_NO);
+%     json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+%     out = json_2_ints(json_out);  ret = out(1);
+%     assert(logical(ret), sprintf('Error in %s', 'SaveFormatFile: get_record_rate'));
+%     record_rate = out(2)
+%     % Open File
+%     cm = sprintf('mraw_file_save_open %d %d %s %d %d', DEVICE_NO, CHILD_NO, file_path, PDC_MRAW_BITDEPTH_16, 0);
+%     json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+%     out = json_2_ints(json_out);  ret = out(1);
+%     assert(logical(ret), sprintf('Error in %s', 'SaveFormatFile: mraw_file_save_open'));
+%     % Load Images to file
+%     for i=1:SAVEDFRAMES+1
+%         frameNo = startNo + int32(i - 1);
+%         if frameNo < 0
+%             frameNo = endNo + abs(startNo) + frameNo + int32(1);
+%         end    
+%         cm = sprintf('mraw_file_save %d %d %d', DEVICE_NO, CHILDE_NO, frameNo);
+%         json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+%         out = json_2_ints(json_out);  ret = out(1);
+%         if ret == PDC_FAILED
+%             cm = sprintf('mraw_file_save_close %d %d', DEVICE_NO, CHILD_NO);
+%             json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+%             out = json_2_ints(json_out);  ret = out(1);
+%             error(2,'Error in %s', 'SaveFormatFile: mraw_file_save_open')
+%         end
+%     end
+%     cm = sprintf('mraw_file_save_close %d %d', DEVICE_NO, CHILD_NO);
+%     json_out = sdk.send_command(pyargs('instrument_id', cam_id, 'command_invocation', cm));
+%     out = json_2_ints(json_out);  ret = out(1);
+% end
