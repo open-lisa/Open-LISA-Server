6a08314da5eda51dde8965a18a952e618c6777f8
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-10-20 22:15:23 -0300

    create folders

--- open_lisa/config/config.py
+++ open_lisa/config/config.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import logging
+from pathlib import Path
 
 
 def load_config(env):
@@ -8,3 +9,10 @@ def load_config(env):
     logging.info(
         "[OpenLISA][load_config] - Loading env variables from {}".format(cfg_path))
     load_dotenv(cfg_path)
+
+    # create folders if they no exist
+    logging.info(
+        "[OpenLISA][load_config] - creating folders")
+    Path(os.getenv("DATABASE_FOLDER")).mkdir(parents=True, exist_ok=True)
+    Path(os.getenv("CLIBS_FOLDER")).mkdir(parents=True, exist_ok=True)
+    Path(os.getenv("USER_FILES_FOLDER")).mkdir(parents=True, exist_ok=True)
